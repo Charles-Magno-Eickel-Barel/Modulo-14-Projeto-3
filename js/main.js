cc258bae7cb0877fd40b2a9cd0e4a4a883662a7a
seu-usersaneme <seu-email>
2023-01-05 06:47:25 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: false,
+            },
+            mensagem: {
+                required: true,
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            email: 'Por favor, insira o seu e-mail',
+            mensagem: 'Por favor, insira o seu nome'
+        },
+        subimitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        },
+    })
+
+    $('#bandeira-da-revolução button').click(function(){
+        const destino = $('#historia');
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        },1000)
+    })
+})
